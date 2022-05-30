f4859676d7b6701385982a8e928745144991fd7c
Gustavo Duarte <gduartemenezes@gmail.com>
2022-05-30 08:56:06 -0300

    feat: add playlist route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { getWeatherController } from './useCases/GetWeatherContidions'
+import { createPlaylistController } from './useCases/CreatePlaylist'
 
 const router = Router()
 
 router.get('/weather', async (request, response) => {
-  console.log(request.query)
   return await getWeatherController.handle(request, response)
+})
+router.get('/playlist', async (request, response) => {
+  return await createPlaylistController.handle(request, response)
 })
 
 export { router }
