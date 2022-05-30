64255fdd08d7e4fedc634a9444242b7d961ae03f
Gustavo Duarte <gduartemenezes@gmail.com>
2022-05-30 08:49:50 -0300

    feat: import cors and dotenv

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { router } from './routes'
-
+import cors from 'cors'
+import 'dotenv/config'
 const app = express()
 
+app.use(cors({}))
 app.use(express.json())
 app.use(router)
 export { app }
