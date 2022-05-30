bbb9b3ad7098cb4e43099bb72d0a8aa88afea59c
Gustavo Duarte <gduartemenezes@gmail.com>
2022-05-30 08:51:44 -0300

    feat: use env variables to provide keys

--- src/providers/implementations/WeatherProvider.ts
+++ src/providers/implementations/WeatherProvider.ts
@@ -2,16 +2,15 @@
 import { Weather } from '../../entities/Weather'
 import { IWeatherProvider } from '../IWeatherProvider'
 import axios from 'axios'
-import ThirdPartyKeys from '../../utils/third-party-keys.json'
 export class WeatherProvider implements IWeatherProvider {
   async findByCity (city: string): Promise<Weather> {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${ThirdPartyKeys.openWeather}`)
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_KEY}`)
     console.log(response.data)
     return response.data
   }
 
   async findByCoordinates (lat: string, lon: string): Promise<Weather> {
-    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${ThirdPartyKeys.openWeather}`)
+    const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPEN_WEATHER_KEY}`)
     console.log(response.data)
     return response.data
   }
